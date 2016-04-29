b7232ce98b7624084906bdc2405fd9efb8cd5010
Connor Peshek <Connorpeshek@gmail.com>
2016-04-29 16:57:51 -0500

    add main code

--- app.js
+++ app.js
@@ -3,7 +3,49 @@
 
   let options = INSTALL_OPTIONS
 
+  let previousSelectionString
+  let tooltip
+
+  // this is good stuff. no matter what, we need a link. let's do it asap
+  // 
+  const link = document.createElement("a")
+
+  link.className = "eager-tweet-this"
+  link.innerText = "Tweet this!"
+  link.target = "_blank"
+
+  function clearTooltip() {
+    tooltip && tooltip.remove()
+  }
+
+  function handleMouseUp() {
+    const selection = window.getSelection()
+    const selectionString = selection.toString()
+
+    if (selection.type !== "Range" || previousSelectionString === selectionString) {
+      clearTooltip()
+      return
+    }
+
+    previousSelectionString = selectionString
+
+    link.href = `https://twitter.com/intent/tweet?text=${encodeURI(selectionString)}`
+
+    tooltip = new window.Tooltip({
+      target: selection.anchorNode.parentNode,
+      position: "top center",
+      content: link,
+      openOn: "always"
+    })
+  }
+
   function updateElement() {
+    link.addEventListener("click", event => {
+      console.log('link clicked')
+      event.stopPropagation()
+      clearTooltip()
+    })
+    document.addEventListener("mouseup", handleMouseUp)
   }
 
   if (document.readyState === "loading") {
--- build/app.js
+++ build/app.js
@@ -5,7 +5,50 @@
 
   var options = INSTALL_OPTIONS;
 
-  function updateElement() {}
+  var previousSelectionString = void 0;
+  var tooltip = void 0;
+
+  // this is good stuff. no matter what, we need a link. let's do it asap
+  //
+  var link = document.createElement("a");
+
+  link.className = "eager-tweet-this";
+  link.innerText = "Tweet this!";
+  link.target = "_blank";
+
+  function clearTooltip() {
+    tooltip && tooltip.remove();
+  }
+
+  function handleMouseUp() {
+    var selection = window.getSelection();
+    var selectionString = selection.toString();
+
+    if (selection.type !== "Range" || previousSelectionString === selectionString) {
+      clearTooltip();
+      return;
+    }
+
+    previousSelectionString = selectionString;
+
+    link.href = "https://twitter.com/intent/tweet?text=" + encodeURI(selectionString);
+
+    tooltip = new window.Tooltip({
+      target: selection.anchorNode.parentNode,
+      position: "top center",
+      content: link,
+      openOn: "always"
+    });
+  }
+
+  function updateElement() {
+    link.addEventListener("click", function (event) {
+      console.log('link clicked');
+      event.stopPropagation();
+      clearTooltip();
+    });
+    document.addEventListener("mouseup", handleMouseUp);
+  }
 
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", updateElement);
