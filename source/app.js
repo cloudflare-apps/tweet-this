926908cfb8e140a483873ec6b1a8381d1b2c9643
Teffen Ellis (Eric) <teffen@nirri.us>
2017-04-20 16:34:32 -0700

    CF v2 migration.

--- source/app.js
+++ source/app.js
@@ -4,13 +4,13 @@
   const {max} = Math
   const CHARACTER_LIMIT = 140
   const URL_LENGTH = 23
-  const TRUNCATE_CHARACTER = "...”"
-  const URL_DELIMITER = " — "
+  const TRUNCATE_CHARACTER = '...”'
+  const URL_DELIMITER = ' — '
 
   const modalConfig = {
-    scrollbars: "yes",
-    toolbar: "no",
-    location: "yes",
+    scrollbars: 'yes',
+    toolbar: 'no',
+    location: 'yes',
     width: 670,
     height: 472
   }
@@ -24,29 +24,27 @@
     <path d="M25.07,14.374 C25.078,14.53 25.082,14.686 25.082,14.847 C25.082,19.667 21.412,25.226 14.701,25.226 C12.64,25.226 10.724,24.622 9.109,23.587 C9.396,23.62 9.685,23.638 9.978,23.638 C11.689,23.638 13.261,23.056 14.509,22.076 C12.913,22.047 11.566,20.992 11.103,19.543 C11.324,19.584 11.552,19.609 11.789,19.609 C12.121,19.609 12.443,19.562 12.75,19.48 C11.082,19.144 9.822,17.669 9.822,15.904 L9.822,15.857 C10.316,16.13 10.879,16.295 11.476,16.314 C10.497,15.66 9.853,14.543 9.853,13.277 C9.853,12.609 10.033,11.98 10.347,11.443 C12.146,13.65 14.835,15.103 17.867,15.254 C17.805,14.988 17.771,14.711 17.771,14.424 C17.771,12.408 19.406,10.778 21.419,10.778 C22.468,10.778 23.417,11.219 24.083,11.928 C24.913,11.764 25.694,11.461 26.399,11.041 C26.126,11.895 25.547,12.609 24.794,13.061 C25.534,12.973 26.237,12.776 26.89,12.489 C26.402,13.218 25.783,13.86 25.07,14.374 L25.07,14.374 Z" fill="#FEFEFE"></path>
   </svg>`
 
-  function clearTooltip() {
-    previousSelectionString = ""
+  function clearTooltip () {
+    previousSelectionString = ''
 
     tooltip && tooltip.remove()
   }
 
-  function getMessage() {
-    let username = options.username.enabled && options.username.value.trim() || ""
+  function getMessage () {
+    let username = options.username.enabled ? options.username.value.trim() : ''
     let message = `“${selectionString.trim()}”`
-    let url = ""
+    let url = ''
 
     if (username) username = ` ${options.username.value}`
 
-    if (options.url.type === "custom") {
+    if (options.url.type === 'custom') {
       url = options.url.custom
-    }
-    else if (options.url.type === "location") {
-      if (INSTALL_ID === "preview") {
-        const {host, path, scheme} = Eager.proxy.originalURL.parsed
+    } else if (options.url.type === 'location') {
+      if (INSTALL_ID === 'preview') {
+        const {host, path, scheme} = INSTALL.proxy.originalURL.parsed
 
         url = `${scheme}://${host}${path}`
-      }
-      else {
+      } else {
         url = window.location
       }
     }
@@ -66,7 +64,8 @@
     return message.trim()
   }
 
-  window.openEagerTweetPopup = function openEagerTweetPopup() {
+  // Setting this method on the global fixes several cross-browser pop-up issues.
+  window.openEagerTweetPopup = function openEagerTweetPopup () {
     const w = window
     const selection = window.getSelection()
     const message = getMessage()
@@ -82,12 +81,12 @@
     const features = Object
       .keys(modalConfig)
       .map(key => `${key}=${modalConfig[key]}`)
-      .join(",")
+      .join(',')
 
-    window.open(`https://twitter.com/intent/tweet?text=${encodeURI(message)}`, "_blank", features)
+    window.open(`https://twitter.com/intent/tweet?text=${encodeURI(message)}`, '_blank', features)
   }
 
-  function updateTooltip(forceVisibility = false) {
+  function updateTooltip (forceVisibility = false) {
     const text = options.text.trim()
     const selection = window.getSelection()
 
@@ -97,50 +96,48 @@
 
     if ((selection.isCollapsed || !selectionHasChanged) && !forceVisibility) {
       clearTooltip()
-    }
-    else if (!selection.isCollapsed && (selectionHasChanged || forceVisibility)) {
+    } else if (!selection.isCollapsed && (selectionHasChanged || forceVisibility)) {
       clearTooltip()
 
       previousSelectionString = selectionString
 
-      const innerLink = document.createElement("a")
+      const innerLink = document.createElement('a')
 
       innerLink.innerHTML = text ? `${text} ${BIRD}` : BIRD
-      innerLink.href = "javascript:openEagerTweetPopup()" // eslint-disable-line no-script-url
-      innerLink.className = "eager-tt-content"
+      innerLink.href = 'javascript:openEagerTweetPopup()'
+      innerLink.className = 'eager-tt-content'
 
       tooltip = new window.Tooltip({
-        classes: "eager-tweet-this",
-        content: document.createElement("div"),
-        openOn: "always",
-        position: "top center",
+        classes: 'eager-tweet-this',
+        content: document.createElement('div'),
+        openOn: 'always',
+        position: 'top center',
         target: selection.anchorNode.parentNode
       })
 
       const {drop} = tooltip.drop
-      const vendorContent = drop.querySelector(".eager-tt-content")
+      const vendorContent = drop.querySelector('.eager-tt-content')
 
       drop.removeChild(vendorContent)
       drop.appendChild(innerLink)
     }
   }
 
-  function bootstrap() {
-    document.addEventListener("click", () => updateTooltip())
+  function bootstrap () {
+    document.addEventListener('click', () => updateTooltip())
   }
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", bootstrap)
-  }
-  else {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', bootstrap)
+  } else {
     bootstrap()
   }
 
   window.INSTALL_SCOPE = {
-    setOptionsCommon(nextOptions) {
+    setOptionsCommon (nextOptions) {
       options = nextOptions
     },
-    setOptionsRerender(nextOptions) {
+    setOptionsRerender (nextOptions) {
       options = nextOptions
 
       updateTooltip(true)
